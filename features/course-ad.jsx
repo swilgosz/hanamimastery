946dea5442ea3bcdc3dd40407921aaf4c02c2800
Adam Szwaba <szwaba.adam@gmail.com>
2021-04-11 19:11:15 +0200

    Describe Course Ad

--- features/course-ad.jsx
+++ features/course-ad.jsx
@@ -1,30 +1,52 @@
-export default function CourseAd({ bg, size }) {
+import { Box, Button, Grid, Typography, makeStyles } from '@material-ui/core';
+import NextLink from 'next/link';
+
+const useStyles = makeStyles((theme) => ({
+  linkButton: {
+    color: theme.palette.common.white,
+  },
+}));
+
+export default function CourseAd({ BoxProps = {}, size = 'big', bg }) {
+  const classes = useStyles();
   return (
-    <div className="columns" style={{ backgroundColor: bg }}>
-      <div className="column is-one-half">
-        <img src="/rails-rest-api.jpg" alt="Rails api thumbnail" />
-      </div>
-      <div className="column is-one-half">
-        <div className="content">
-          <h2 className="title">Ruby On Rails REST API</h2>
-          <h3 className={`subtitle ${size === 'small' ? 'is-4' : 'is-5'}`}>
-            The complete guide
-          </h3>
-          <p className={size === 'small' ? 'is-size-6' : 'is-size-3'}>
-            Create professional API applications that you can hook anything
-            into! Learn how to code like professionals using Test Driven
-            Development!
-          </p>
-        </div>
-        <a
+    <Grid
+      spacing={3}
+      container
+      component={(props) => <Box {...BoxProps} {...props} bgcolor={bg || ''} />}
+    >
+      <Grid
+        item
+        xs={6}
+        component="img"
+        src="/rails-rest-api.jpg"
+        alt="Rails api thumbnail"
+      />
+      <Grid item xs={6}>
+        <Typography variant="h4">Ruby On Rails REST API</Typography>
+        <Typography variant={size === 'small' ? 'subtitle2' : 'h6'}>
+          The complete guide
+        </Typography>
+        <p className={size === 'small' ? 'is-size-6' : 'is-size-3'}>
+          Create professional API applications that you can hook anything into!
+          Learn how to code like professionals using Test Driven Development!
+        </p>
+        <NextLink
           href="https://www.udemy.com/ruby-on-rails-api-the-complete-guide/?couponCode=DGLWEB"
-          target="_blank"
-          className={size === 'small' ? 'is-medium' : 'is-large'}
-          rel="noreferrer"
+          passHref
         >
-          Take this course!
-        </a>
-      </div>
-    </div>
+          <Button
+            className={classes.linkButton}
+            variant="contained"
+            color="primary"
+            size={size === 'small' ? 'medium' : 'large'}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Take this course!
+          </Button>
+        </NextLink>
+      </Grid>
+    </Grid>
   );
 }
