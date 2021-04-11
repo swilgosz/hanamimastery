c4f29880350dbfc3430739166327d842a75e40b8
Adam Szwaba <szwaba.adam@gmail.com>
2021-04-11 16:41:47 +0200

    Place TopNav on top of every page

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../styles/theme';
+import React from "react";
+import PropTypes from "prop-types";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TopNav from "../features/top-nav/index";
+import theme from "../styles/theme";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
 
   return (
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <TopNav />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
