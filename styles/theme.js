824e07f5b8a7378d27776ae18656070d8d1893ec
Adam Szwaba <szwaba.adam@gmail.com>
2021-04-11 16:38:29 +0200

    Add main theme color

--- styles/theme.js
+++ styles/theme.js
@@ -1,4 +1,4 @@
 import { createMuiTheme } from "@material-ui/core";
+import { orange } from "@material-ui/core/colors";
 
-
-export default createMuiTheme();
+export default createMuiTheme({ palette: { primary: orange } });
