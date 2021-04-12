3d5ea2c5bb45a7a383c0cf39c24e54e9b5deeda1
Adam Szwaba <szwaba.adam@gmail.com>
2021-04-12 10:45:16 +0200

    Update articles slice to compare by date published

--- redux/slices/articles.js
+++ redux/slices/articles.js
@@ -1,6 +1,16 @@
 import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
 
-const articlesAdapter = createEntityAdapter();
+const articlesAdapter = createEntityAdapter({
+  sortComparer: (articleA, articleB) => {
+    const [createdAtA, createdAtB] = [
+      new Date(articleA.attributes.publishedAt),
+      new Date(articleB.attributes.publishedAt),
+    ];
+    if (createdAtA > createdAtB) return -1;
+    if (createdAtA < createdAtB) return 1;
+    return 0;
+  },
+});
 
 const articlesSlice = createSlice({
   name: 'articles',
