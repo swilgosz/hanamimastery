b13eeee4362bc791b0ad4c8a8cd9c273cbf29127
Adam Szwaba <szwaba.adam@gmail.com>
2021-04-13 14:41:12 +0200

    Add disqus

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,9 +1,93 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import hydrate from 'next-mdx-remote/hydrate';
+import renderToString from 'next-mdx-remote/render-to-string';
+import rehypePrism from '@mapbox/rehype-prism';
+import fs from 'fs';
+import path from 'path';
+import readingTime from 'reading-time';
+import { Box, Container, Grid, Typography } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import { DiscussionEmbed } from 'disqus-react';
 import getArticlesData from '../../utils/get-articles-data';
+import components from '../../features/mdx-components';
 
-export default function BlogIndex({ data }) {
+const getArticleSourceFromFile = async (slug) => {
+  const file = fs.readFileSync(
+    path.join(`${process.cwd()}/articles/${slug}.mdx`)
+  );
+  return file;
+};
+
+const getArticleData = async (slug) => {
+  try {
+    const res = await fetch(
+      `https://api.sourcerio.com/blogging/v1/blogs/driggl/articles/${slug}`
+    );
+    const data = await res.json();
+    if (!data) throw new Error();
+
+    const {
+      publishedAt,
+      status,
+      metadata,
+      excerpt,
+      content,
+      title,
+      thumbnail,
+    } = data.data.attributes;
+
+    const author = data.data.relationships.author.data;
+    const article = {
+      id: data.data.id,
+      author,
+      publishedAt,
+      status,
+      slug,
+      metadata,
+      excerpt,
+      title,
+      thumbnail,
+      readingTime: readingTime(content),
+    };
+
+    const articleSource =
+      process.env.ARTICLES_SOURCE === 'API'
+        ? content
+        : await getArticleSourceFromFile(slug);
+
+    const mdxSource = await renderToString(articleSource, {
+      components,
+      mdxOptions: {
+        remarkPlugins: [],
+        rehypePlugins: [rehypePrism],
+      },
+    });
+    return { source: mdxSource, article };
+  } catch (error) {
+    return undefined;
+  }
+};
+
+export default function BlogIndex({ source, article }) {
+  const content = hydrate(source, { components });
+  const { slug, title, thumbnail, id } = article;
   return (
-    <div dangerouslySetInnerHTML={{ __html: data.data.attributes.content }} />
+    <>
+      <Box component="img" src={thumbnail.full} width="100%" />
+      <Container maxWidth="lg" component="main">
+        <Typography variant="h2">{title}</Typography>
+        {content}
+        <DiscussionEmbed
+          shortname="driggl"
+          config={{
+            url: `https://driggl.com/blog/a/${slug}?comments-version=2`,
+            title,
+            identifier: `article-${id}-v2`,
+          }}
+        />
+      </Container>
+    </>
   );
 }
 
@@ -12,23 +96,20 @@ export async function getStaticPaths() {
   const paths = Object.values(articles).map((article) => ({
     params: { slug: article.attributes.slug },
   }));
-  console.log(paths);
   return { paths, fallback: false };
 }
 
 export async function getStaticProps(context) {
-  const res = await fetch(
-    `https://api.sourcerio.com/blogging/v1/blogs/driggl/articles/${context.params.slug}`
-  );
-  const data = await res.json();
-
-  if (!data) {
+  const articleData = await getArticleData(context.params.slug);
+  if (!articleData) {
     return {
       notFound: true,
     };
   }
 
+  const { source, article } = articleData;
+
   return {
-    props: { data }, // will be passed to the page component as props
+    props: { source, article }, // will be passed to the page component as props
   };
 }
