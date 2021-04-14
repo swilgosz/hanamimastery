c41f6206e08de0a465b4558dae1ca0f794342c2c
Adam Szwaba <szwaba.adam@gmail.com>
2021-04-14 08:00:59 +0200

    Add main article layout

---
+++ layouts/article-layout.jsx
@@ -0,0 +1,33 @@
+import { Grid, Container, makeStyles } from '@material-ui/core';
+import EmailSubscriptionForm from '../features/email-subscription-form';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const ArticleLayout = ({ aside, article }) => {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      className={classes.root}
+      component={(props) => (
+        <Container maxWidth="lg" component="main" {...props} />
+      )}
+    >
+      <Grid item xs={12} md={2} component="aside">
+        {aside}
+      </Grid>
+      <Grid item xs={12} md={8} component="article">
+        {article}
+      </Grid>
+      <Grid item xs={12} md={2}>
+        <EmailSubscriptionForm />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ArticleLayout;
