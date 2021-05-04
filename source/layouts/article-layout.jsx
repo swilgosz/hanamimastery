4dc9f5042e5847abf8ac7c1f500f05a4eb601e9a
Sebastian Wilgosz <sebastian@driggl.com>
2021-05-04 03:21:19 +0200

    Add support button to sidebar

--- source/layouts/article-layout.jsx
+++ source/layouts/article-layout.jsx
@@ -1,3 +1,10 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardHeader,
+} from '@material-ui/core';
 import { Grid, Container, makeStyles } from '@material-ui/core';
 import EmailSubscriptionForm from '../features/email-subscription-form';
 
@@ -21,6 +28,35 @@ const ArticleLayout = ({ aside, article }) => {
         {article}
       </Grid>
       <Grid item xs={12} md={4}>
+        <Card>
+          <CardHeader
+            disableTypography
+            title={
+              <Typography variant="h5">
+                Sponsor this project on Github!
+              </Typography>
+            }
+            subheader={
+              <>
+                <Typography variant="subtitle1">
+                  10% of all your support goes to Hanami development support
+                </Typography>
+              </>
+            }
+          />
+          <CardContent>
+            <Typography>
+              <iframe
+                src="https://ghbtns.com/github-btn.html?user=swilgosz&type=sponsor"
+                frameborder="0"
+                scrolling="0"
+                width="150"
+                height="20"
+                title="GitHub" /
+                >
+            </Typography>
+          </CardContent>
+        </Card>
         <EmailSubscriptionForm />
       </Grid>
     </Grid>
