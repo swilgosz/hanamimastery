eb0189748ed57266d00ad457361f242e60f9d73f
Adam Szwaba <szwaba.adam@gmail.com>
2021-05-06 18:00:01 +0200

    Add styling to email form

--- features/email-subscription-form/index.jsx
+++ features/email-subscription-form/index.jsx
@@ -1,4 +1,22 @@
 import React from "react";
+import { Card, CardContent, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(2),
+    [theme.breakpoints.down("md")]: {
+      margin: theme.spacing(2, 4),
+    },
+  },
+}));
+
 export default function EmailSubscriptionForm() {
-  return <div id="om-jygtzgejjk4smsfhwyo7-holder"></div>;
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <div id="om-jygtzgejjk4smsfhwyo7-holder"></div>
+      </CardContent>
+    </Card>
+  );
 }
