5c5a4762b727856f06f56725f19ff4f3c60800fe
Adam Szwaba <szwaba.adam@gmail.com>
2021-05-06 13:49:54 +0200

    Fix get page data

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,46 +1,44 @@
-import React from 'react';
-import hydrate from 'next-mdx-remote/hydrate';
-import { Container, Typography, makeStyles } from '@material-ui/core';
-import { NextSeo } from 'next-seo';
-import getPageData from '../utils/get-page-data';
-import components from '../features/mdx-components';
-import ArticleLayout from '../layouts/article-layout';
+import React from "react";
+import hydrate from "next-mdx-remote/hydrate";
+import { Container, Typography, makeStyles } from "@material-ui/core";
+import { NextSeo } from "next-seo";
+import getPageData from "../utils/get-page-data";
+import components from "../features/mdx-components";
+import ArticleLayout from "../layouts/article-layout";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
     padding: 0,
     minHeight: theme.spacing(6),
   },
 }));
 
-export default function Page({ page }) {
+export default function Page({ page, content }) {
+  console.log(page, content);
   const classes = useStyles();
-  const content = hydrate(page.content, { components });
+  const pageContent = hydrate(content, { components });
   const { slug, title, thumbnail, id, excerpt } = page;
   const url = `${process.env.NEXT_PUBLIC_BASE_URL}/episodes/${slug}`;
 
   return (
     <>
-    <NextSeo
-    title="About | Hanami Mastery - the concept and mission."
-    titleTemplate="%s | Hanami Mastery - learn hanami as a pro"
-    description="Discover the rules driving the Hanami Mastery project!"
-    openGraph={{
-      title: 'About - Hanami Mastery',
-      description:
-        'Discover the rules driving the Hanami Mastery project!',
-      images: ['/home-cover.jpg'],
-      type: 'website',
-    }}
-  />
+      <NextSeo
+        title="About | Hanami Mastery - the concept and mission."
+        titleTemplate="%s | Hanami Mastery - learn hanami as a pro"
+        description="Discover the rules driving the Hanami Mastery project!"
+        openGraph={{
+          title: "About - Hanami Mastery",
+          description: "Discover the rules driving the Hanami Mastery project!",
+          images: ["/home-cover.jpg"],
+          type: "website",
+        }}
+      />
 
       <ArticleLayout
         article={
           <Container maxWidth="lg" component="main">
-            <article >
-              {content}
-            </article>
+            <article>{pageContent}</article>
           </Container>
         }
       />
@@ -49,7 +47,7 @@ export default function Page({ page }) {
 }
 
 export async function getStaticProps() {
-  const pageData = await getPageData('about');
+  const pageData = await getPageData("about");
 
   if (!pageData) {
     return {
@@ -57,10 +55,10 @@ export async function getStaticProps() {
     };
   }
 
-  const { page } = pageData
+  const { page, content } = pageData;
 
   return {
-    props: { page }, // will be passed to the page component as props
+    props: { page, content }, // will be passed to the page component as props
     revalidate: 604800, // revalidate the page every week
   };
 }
