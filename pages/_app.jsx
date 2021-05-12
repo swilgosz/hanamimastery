e157482a5ec06f246f73fba47b5c983c83ca9128
Sebastian Wilgosz <sebastian@driggl.com>
2021-05-12 11:40:00 +0200

    Revert "Add GTM script"
    This reverts commit 0d9df334c4adf54bbe9eb0cf49195dc82fb17a71.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -28,10 +28,6 @@ export default function MyApp(props) {
     }
   }, []);
 
-  React.useEffect(() => {
-    TagManager.initialize({ gtmId: `${process.env.NEXT_PUBLIC_GTM_ID}` });
-  }, []);
-
   return (
     <Provider store={store}>
       <Head>
