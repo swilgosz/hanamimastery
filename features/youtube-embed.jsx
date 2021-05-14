02c72360176ae8fa84f82aa2a5a15c14af4fa791
Sebastian Wilgosz <sebastian@driggl.com>
2021-05-15 01:30:44 +0200

    Fix yt component to only render when the id is present

--- features/youtube-embed.jsx
+++ features/youtube-embed.jsx
@@ -1,21 +1,22 @@
 import PropTypes from "prop-types";
 
-const YoutubeEmbed = ({ embedId }) => (
-  <div className="video-responsive">
-    <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
-);
-
-YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired
+const YoutubeEmbed = ({ embedId }) => {
+  if (!embedId) {
+    return '';
+  } else {
+    return <div className="video-responsive">
+      <iframe
+        width="853"
+        height="480"
+        src={`https://www.youtube.com/embed/${embedId}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  }
 };
+
 
 export default YoutubeEmbed;
