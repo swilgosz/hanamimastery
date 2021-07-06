1cc310cb5721cc9d8f5eb7b65a22fbb59a2d10be
Sebastian Wilgosz <sebastian@driggl.com>
2021-07-06 02:05:24 +0200

    Add rss to the home page

--- features/top-nav/menu-items.jsx
+++ features/top-nav/menu-items.jsx
@@ -1,20 +1,33 @@
 import NextLink from "next/link";
 import MenuItem from "@material-ui/core/MenuItem";
 import { nanoid } from "@reduxjs/toolkit";
+import RssFeedIcon from '@material-ui/icons/RssFeed';
 
 const links = [
   { href: "/c/stray", label: "Stray" },
   { href: "/about", label: "About" },
   { href: "/sponsors", label: "Sponsors" },
+  { href: "/feed", label: "RSS", icon: '/rss-feed-icon.png'}
 ];
 
 const MenuItems = ({ MenuItemProps = {} }) =>
-  links.map(({ href, label }) => (
-    <NextLink href={href}>
-      <MenuItem key={nanoid()} {...MenuItemProps}>
-        {label}
-      </MenuItem>
-    </NextLink>
-  ));
+  links.map(({ href, label, icon }) => {
+    if (icon) {
+      return (
+        <NextLink href={href}>
+          <MenuItem key={nanoid()} {...MenuItemProps}>
+            <RssFeedIcon />
+          </MenuItem>
+      </NextLink>
+      );
+    }
+    return (
+      <NextLink href={href}>
+        <MenuItem key={nanoid()} {...MenuItemProps}>
+          {label}
+        </MenuItem>
+      </NextLink>
+    )
+  });
 
 export default MenuItems;
