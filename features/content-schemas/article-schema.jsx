854779cb48c6b4794d216ccc9ec9f599b6eba5fd
Sebastian Wilgosz <sebastian@driggl.com>
2021-10-03 20:53:00 +0200

    Add author and publisher and more video information to episode structured data

--- features/content-schemas/article-schema.jsx
+++ features/content-schemas/article-schema.jsx
@@ -12,11 +12,28 @@ function makeArticleSchema(article) {
       description: article.excerpt,
       keywords: article.tags.toString(),
       name: article.title,
+      headline: article.title,
       image: `${process.env.NEXT_PUBLIC_BASE_URL}${article.thumbnail.big}`,
       alternateName: article.alias,
       inLanguage: 'en-US',
+      author: {
+        '@type': 'Person',
+        name: 'Sebastian Wilgosz',
+        url: 'https://twitter.com/sebwilgosz',
+        image: `${process.env.NEXT_PUBLIC_BASE_URL}/images/team/swilgosz-medium.jpg`,
+        description: "A creator of HanamiMastery, productivity madman. I love to make others' lives better as mine is quite good already:) My Bio links: https://bio.link/swilgosz"
+      },
+      publisher: {
+        '@type': "Organization",
+        name: 'Hanami Mastery',
+        url: 'https://hanamimastery.com'
+      },
       video: {
         '@type': 'VideoObject',
+        name: article.title,
+        description: article.excerpt,
+        uploadDate: article.publishedAt,
+        thumbnailUrl: `${process.env.NEXT_PUBLIC_BASE_URL}${article.thumbnail.big}`,
         embedUrl: `https://www.youtube.com/embed/${article.videoId}`,
         datePublished: article.publishedAt,
         inLanguage: 'en-US'
