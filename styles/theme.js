2fe0825be62df2f4b966e47c210467009aed2728
Daishi00 <hasiuk9@gmail.com>
2023-05-30 10:19:32 +0200

    add custom breakpoints

--- styles/theme.js
+++ styles/theme.js
@@ -6,6 +6,15 @@ export default createTheme({
       main: '#CB4426',
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1280,
+      xl: 1536,
+    },
+  },
   components: {
     MuiLink: {
       styleOverrides: {
