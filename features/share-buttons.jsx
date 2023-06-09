32ea5e1de26166a852b9bcdfae224d3e595e94db
Sebastian Wilgosz <sebastian@hanamimastery.com>
2023-06-09 15:28:23 +0200

    Revert "Merge pull request #53 from swilgosz/fix_sharethis_script"
    This reverts commit 1d84ad8097e24b9f212e0e193b2e6492f0c662ea, reversing
    changes made to 4ace0c4b8ca1d2937a7f993126b58d58a3ec1c51.

--- features/share-buttons.jsx
+++ features/share-buttons.jsx
@@ -1,46 +1,42 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { StickyShareButtons } from 'sharethis-reactjs';
-import Script from 'next/script';
 
 export default function ShareButtons({ data }) {
   const { url, thumbnail, title, id } = data;
 
   return (
-    <>
-      <Script src="https://platform-api.sharethis.com/js/sharethis.js#property=60b823ffeed0fd001128d645&product=sticky-share-buttons" />
-      <StickyShareButtons
-        key={id}
-        config={{
-          alignment: 'right', // alignment of buttons (left, right)
-          color: 'social', // set the color of buttons (social, white)
-          enabled: true, // show/hide buttons (true, false)
-          font_size: 14, // font size for the buttons
-          hide_desktop: false, // hide buttons on desktop (true, false)
-          labels: 'cta', // button labels (cta, counts, null)
-          language: 'en', // which language to use (see LANGUAGES)
-          min_count: 1, // hide react counts less than min_count (INTEGER)
-          networks: [
-            // which networks to include (see SHARING NETWORKS)
-            'twitter',
-            'reddit',
-            'linkedin',
-            'facebook',
-            'pinterest',
-            'email',
-          ],
-          padding: 12, // padding within buttons (INTEGER)
-          radius: 4, // the corner radius on each button (INTEGER)
-          show_total: true, // show/hide the total share count (true, false)
-          show_mobile: true, // show/hide the buttons on mobile (true, false)
-          show_toggle: true, // show/hide the toggle buttons (true, false)
-          size: 48, // the size of each button (INTEGER)
-          top: 160, // offset in pixels from the top of the page
-          url,
-          title,
-          image: thumbnail.small,
-        }}
-      />
-    </>
+    <StickyShareButtons
+      key={id}
+      config={{
+        alignment: 'right', // alignment of buttons (left, right)
+        color: 'social', // set the color of buttons (social, white)
+        enabled: true, // show/hide buttons (true, false)
+        font_size: 14, // font size for the buttons
+        hide_desktop: false, // hide buttons on desktop (true, false)
+        labels: 'cta', // button labels (cta, counts, null)
+        language: 'en', // which language to use (see LANGUAGES)
+        min_count: 1, // hide react counts less than min_count (INTEGER)
+        networks: [
+          // which networks to include (see SHARING NETWORKS)
+          'twitter',
+          'reddit',
+          'linkedin',
+          'facebook',
+          'pinterest',
+          'email',
+        ],
+        padding: 12, // padding within buttons (INTEGER)
+        radius: 4, // the corner radius on each button (INTEGER)
+        show_total: true, // show/hide the total share count (true, false)
+        show_mobile: true, // show/hide the buttons on mobile (true, false)
+        show_toggle: true, // show/hide the toggle buttons (true, false)
+        size: 48, // the size of each button (INTEGER)
+        top: 160, // offset in pixels from the top of the page
+        url,
+        title,
+        image: thumbnail.small,
+      }}
+    />
   );
 }
